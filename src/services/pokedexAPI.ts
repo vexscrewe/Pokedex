a170ce6a03b2b3bc7fc85f5a1ff3628f6d24a583
vexscrewe <monokumaphu@gmail.com>
2025-05-25 20:57:11 -0400

    AtualizaçãoAPI

--- src/services/pokedexAPI.ts
+++ src/services/pokedexAPI.ts
@@ -7,7 +7,7 @@ interface PokeResponse {
 }
 
 const api = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon'
+    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
 })
 
 export async function fetchPokes (search = '') {
